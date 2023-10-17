a91f452c9320479d8d54f2aeab2359ec7947730d
cosmas <cosmasthuku4@gmail.com>
2023-10-17 14:31:01 +0300

    loadash update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     modules: false,
-//   },
-// })
-export default {
+//https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
   css: {
     modules: false,
   },
-  resolve: {
-    alias: {
-      // Add aliases for lodash modules to resolve them correctly
-      'lodash/assign': './node_modules/lodash/assign.js', // You may need to adjust this path based on your project structure
-    },
-  },
-};
+})
+// export default {
+//   plugins: [react()],
+//   css: {
+//     modules: false,
+//   },
+//   resolve: {
+//     alias: {
+//       // Add aliases for lodash modules to resolve them correctly
+//       'lodash/assign': './node_modules/lodash/assign.js', // You may need to adjust this path based on your project structure
+//     },
+//   },
+// };
